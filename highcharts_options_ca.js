35ea0f8c46efd3465e839e335bfe83d7ea354f1a
Angel Aguilera <angel.aguilera@gmail.com>
2014-12-18 14:53:20 +0100

    Added the source code

---
+++ highcharts_options_ca.js
@@ -0,0 +1,28 @@
+/*
+    Catalan localization options for HighCharts.
+
+    Angel Aguilera <angel.aguilera@gmail.com> (December 2014)
+ 
+    This is free and unencumbered software released into the public domain.
+    See http://unlicense.org
+ */
+Highcharts.setOptions({
+    lang: {
+        contextButtonTitle: "Menú contextual de la gràfica",
+        decimalPoint: ",",
+        downloadJPEG: "Desa com a imatge JPEG",
+        downloadPDF: "Desa com a PDF",
+        downloadPNG: "Desa com a PNG",
+        downloadSVG: "Desa com a SVG",
+        drillUpText: "Torna a {series.name}",
+        loading: "S'està carregant...",
+        monts: ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"],
+        noData: 'No hi ha dades per mostrar',
+        printChart: "Imprimeix la gràfica",
+        resetZoom: "Reinicialitza el nivell d'ampliació",
+        resetZoomTitle: "Reinicialitza el nivell d'ampliació a 1:1",
+        shortMonths: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
+        thousandsSep: ".",
+        weekdays: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"]
+    }
+});
