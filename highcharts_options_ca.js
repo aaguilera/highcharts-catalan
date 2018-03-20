748c497a1a958c69f15cf766180f7726872908c2
Angel Aguilera <angel.aguilera@gmail.com>
2018-03-20 14:48:48 +0100

    Adding attributes for v6.07

--- highcharts_options_ca.js
+++ highcharts_options_ca.js
@@ -8,6 +8,40 @@
  */
 Highcharts.setOptions({
     lang: {
+        accessibility: {
+            axis: {
+                xAxisDescriptionPlural: "El diagrama té {numAxes} eixos X mostrant {#each(names, -1), }i {names[-1]}",
+                xAxisDescriptionSingular: "El diagrama té 1 eix X mostrant {names[0]}.",
+                yAxisDescriptionPlural: "The chart has {numAxes} Y axes displaying {#each(names, -1), }and {names[-1]}",
+                yAxisDescriptionSingular: "El diagrama té 1 eix X mostrant {names[0]}."
+            },
+            chartContainerLabel: "Diagrama interactiu. {title}. Usa les fletxes amunt i avall per navegar amb la majoria de lectors de pantalla.",
+            chartHeading: "Gràfic del diagrama",
+            chartTypes: {
+                barMultiple: "Diagrama de barres amb {numSeries} sèries de dades.",
+                barSingle: "Diagrama de barres amb {numPoints} {#plural(numPoints, bars, bar)}.",
+                boxplotMultiple: "Diagrama de caixa amb {numSeries} sèries de dades.",
+                boxplotSingle: "Diagrama de caixa amb {numPoints} {#plural(numPoints, boxes, box)}.",
+                bubbleMultiple: "Diagrama de bombolles amb {numSeries} sries de dades.",
+                bubbleSingle: "Diagrama de bombolles amb {numPoints} {#plural(numPoints, bubbles, bubble)}.",
+                columnMultiple: "Diagrama de barres amb {numSeries} sèries de dades.",
+                columnSingle: "Diagrama de barres amb {numPoints} {#plural(numPoints, bars, bar)}.",
+                combinationChart: "Diagrama combinat amb {numSeries} sèries de dades.",
+                defaultMultiple: "Diagrama amb {numSeries} sèries de dades.",
+                defaultSingle: "Diagrama amb {numPoints} dades {#plural(numPoints, points, point)}.",
+                emptyChart: "Diagrama buit",
+                lineMultiple: "Diagrama de línies amb {numSeries} línies.",
+                lineSingle: "Diagrama de línies amb {numPoints} dades {#plural(numPoints, points, point)}.",
+                mapTypeDescription: "Mapa de {mapTitle} amb {numSeries} sèries de dades.",
+                pieMultiple: "Diagrama de pastís amb {numSeries} sectors.",
+                pieSingle: "Diagrama de pastís amb {numPoints} {#plural(numPoints, slices, slice)}.",
+                scatterMultiple: "Diagrama de dispersió amb {numSeries} sèries de dades.",
+                scatterSingle: "Diagrama de dispersió amb {numPoints} {#plural(numPoints, points, point)}.",
+                splineMultiple: "Diagram spline amb {numSeries} línies.",
+                splineSingle: "Diagrama spline amb {numPoints} dades {#plural(numPoints, points, point)}.",
+                unknownMap: "Mapa de regió no especificada amb {numSeries} sèries de dades."
+            },
+        },
         contextButtonTitle: "Menú contextual del diagrama",
         decimalPoint: ",",
         downloadCSV: "Desa com a CSV",
@@ -29,6 +63,7 @@ Highcharts.setOptions({
         shortMonths: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
         shortWeekdays: ["Dg", "Dl", "Dm", "Dc", "Dj", "Dv", "Ds"],
         thousandsSep: ".",
+        viewData: "Mostra la taula de dades",
         weekdays: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"]
     }
 });
